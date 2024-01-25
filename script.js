ae4d102f2eeee2c06d98ff413d4f27caaf5beaa2
Autumn Williams <63067844+AutumnHW@users.noreply.github.com>
2024-01-25 15:36:04 -0500

    meh nvm

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var backgroundURL = 0;
 var backgroundID = Math.floor(Math.random() * 7);
-backgroundURL = "url(microsoft/" + backgroundID + ".jpg)";
+backgroundURL = "url(microsoft/" + 0 + ".jpg)";
 document.getElementById('background').style.backgroundImage = backgroundURL;
