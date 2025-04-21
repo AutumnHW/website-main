d3d005fedcc76435bb3cca86f9cbb112558667b9
Autumn Williams <63067844+AutumnHW@users.noreply.github.com>
2025-04-21 17:35:35 -0400

    made deepin

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var backgroundURL = 0;
 var backgroundID = Math.floor(Math.random() * 7);
-backgroundURL = "url(microsoft/" + 0 + ".jpg)";
+backgroundURL = "url(images/background.jpg)";
 document.getElementById('background').style.backgroundImage = backgroundURL;
